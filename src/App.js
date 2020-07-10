701d86f108f08ed89f7dc976243efcc7f15bcbbc
gcmartin <gscalica@gmail.com>
2020-07-10 14:03:19 -0700

    Create components folder and die component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Die from './components/die';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Dice Exercise</h1>
+      <Die />
     </div>
   );
 }
---
+++ src/components/die.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Die() {
+  return(
+    <h1>This is a Die component</h1>
+  );
+}
+
+export default Die;
