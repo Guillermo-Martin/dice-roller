8103c7a3ebc347f3d616af07924af8e1d003b53d
gcmartin <gscalica@gmail.com>
2020-07-10 14:54:15 -0700

    Add FontAwesome CDN and add die icon

--- src/containers/DiceRoller.js
+++ src/containers/DiceRoller.js
@@ -4,7 +4,7 @@ import Die from './../components/die';
 class DiceRoller extends Component {
   // create state to manage the changing of the die's face
   state = {
-    dieNumber: 1
+    dieNumber: <i class="fas fa-dice-one"></i>
   }
 
   // create a function that generates a random number between 1 and 6
